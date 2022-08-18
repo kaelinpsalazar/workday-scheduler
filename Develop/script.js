cf3f95013ccf587ab132c510eec5ca0a2f2561f0
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-08-18 09:39:19 -0600

    adding base code files

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+// 1. Have current time on top of calender
+var today = moment()
+$("#currentDay").text(today.format("MMM Do, YYYY"))
+
+// 2. time blocks for each hour in your html
+
+// 3. color them to indicate past,present, future
+
+// 4. have each time block also be a text field
+
+// 5. each time block should have a save button
+
+// 6. connect local storage so save info can be kept
