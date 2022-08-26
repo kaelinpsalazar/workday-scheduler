e7acca70303656afcba5e8f99bf6f711c09a71db
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-08-26 16:39:23 -0600

    fixed the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var dateToday = $('#currentDay');
 var currentDate = moment().format('MMMM Do YYYY');
 dateToday.text(currentDate);
-var currentTime = moment().hours(); 
+var currentTime = moment().hours();
+ 
 
 var workDay = [
 
@@ -17,7 +18,7 @@ var workDay = [
     17,
     
 ];
-var savedCal=[];
+// var savedCal=[];
 
 for (let i = 0; i < workDay.length; i++) {
     
@@ -38,6 +39,7 @@ for (let i = 0; i < workDay.length; i++) {
 
 
     var textContainer = $('<textarea>');
+    textContainer.val(localStorage.getItem(workDay[i]));
     textContainer.addClass('col-md-8 description').attr("placeholder" ,"Enter Text Here").attr('id','inputValue');
     liEl.append(textContainer);
 
@@ -67,7 +69,7 @@ for (let i = 0; i < workDay.length; i++) {
         
     }
     
-    
+
     // start with a save button event selector.     
     $('button').on('click', function saveFunction(event) {
         event.preventDefault();
@@ -79,16 +81,22 @@ for (let i = 0; i < workDay.length; i++) {
         console.log(textVal)
 
         
-        localStorage.setItem("sectionID",JSON.stringify(sectionID));
-        localStorage.setItem("textVal",JSON.stringify(textVal));
+        localStorage.setItem(sectionID,(textVal));
+        // localStorage.setItem("textVal",JSON.stringify(textVal));
         // use set item to store in local
         
-        JSON.parse(localStorage.getItem(sectionID));
-        JSON.parse(localStorage.getItem(textVal));
-        // use get item to retrieve from storage
-
-        // init() so it loads when page is opened
-
-
+        // JSON.parse(localStorage.getItem(sectionID));
+        // JSON.parse(localStorage.getItem(textVal));
+        // // use get item to retrieve from storage
         
+        // init() so it loads when page is opened
     }) 
+    // 80 let newNote = {sectionId: sectionID, note: textVal,}
+    // 86 let savedNotes = JSON.parse(localStorage.getItem(“notes”))
+
+//     loop to apply code. Clicking on the save buttons saves the time and note for one time block, but when another save button is clicked the information saved in local storage is replaced with that of the new time block. To save all of your notes make a global array variable. Then on line 80 of your script.js create an object variable (example: let newNote = {sectionId: sectionID, note: textVal,}. Then on line 86 of your script.js, change JSON.parse(localStorage.getItem(sectionID)); to let savedNotes = JSON.parse(localStorage.getItem(“notes”));. From there you can delete line 87, and set the textareas content equal to the corresponding note. 
+
+// Based on the grading rubric points were deducted for the following reasons:
+// * Clicking a time block's save button doesn't save the input text to local storage
+// * Code lacks quality comments
+// * Repository README is missing a screenshot and a link to the deployed application
